ca867e600a7d30eaab5ac380a79cfdf8899abb87
HaeriChoi <haeri731@bodyfriend.co.kr>
2024-01-24 09:23:40 +0900

    basic of typescript

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,30 @@
+//JS 문자열 선언
+const str1 = "hello";
+
+//TS 문자열 선언
+const str2: string = "hello";
+
+//TS 숫자
+const num: number = 10;
+
+//TS 배열
+const arr: Array<number> = [1, 2, 3];
+const hearoes: Array<string> = ["Captain", "Thor", "Hulk"];
+const items: number[] = [1, 2, 3, 4];
+
+//TS 튜플
+const address: [string, number] = ["gangnam", 1];
+
+//TS 객체
+const obj: object = {};
+const person1: object = {
+    name: "jellyfish",
+    age: 100,
+};
+const person2: { name: string; age: number } = {
+    name: "thor",
+    age: 1004,
+};
+
+//TS 진위값
+const show: boolean = true;
---
+++ why-ts/sample.js
@@ -0,0 +1,19 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// // 30 !== sum(10, "20"); //1020
+// var result1 = sum(10, 20);
+// result.tolocalestring();
+
+//@ts-check
+
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns {number}
+ */
+function sum(a, b) {
+    return a + b;
+}
+sum(10, "20");
